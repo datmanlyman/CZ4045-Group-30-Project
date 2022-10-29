f0dc33c44cd05f46b247fc8c59210a16c5c78e61
yapsiang <c190077@e.ntu.edu.sg>
2022-10-30 03:34:05 +0800

    updated routing, editted UI, added report page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Data } from './Page/Data';
-import { Report } from './Page/Report';
+import { Data } from "./Page/Data";
+import { Report } from "./Page/Report";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return (
-      <Router>
-        <Routes>
-          <Route index element={<Data/>} />
-          <Route path="report" element={<Report/>} />
-        </Routes>
-      </Router>
-  );
+    return (
+        // <Router>
+        //     <Header />
+        //     <Routes>
+        //         <Route index element={<Data />} />
+        //         <Route path="report" element={<Report />} />
+        //     </Routes>
+        // </Router>
+        <>
+            <Navbar />
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Report />} />
+                    <Route path="/report" element={<Report />} />
+                    <Route path="/data" element={<Data />} />
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 export default App;
