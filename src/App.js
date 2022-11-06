5ea5f987fb43a5e9fc6fc34cd3f365dafd2246ac
datmanlyman <jeewern2@gmail.com>
2022-11-06 20:53:19 +0800

    some random data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Data } from "./Page/Data";
 import { Report } from "./Page/Report";
 import AspectPage from "./Page/AspectPage";
 import Navbar from "./Components/Navbar";
@@ -11,7 +10,6 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Report />} />
-          { /* <Route path="/report" element={<Report />} /> */ }
           <Route path="/dashboard" element={<AspectPage />} />
         </Routes>
       </div>
